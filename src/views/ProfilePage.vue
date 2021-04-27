340257345aa72aad0b3b7dd537d608545c9b4111
daerxi <bli3@unb.ca>
2021-04-27 01:25:06 -0300

    demo review

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <profile-button></profile-button>
-    <div class="p-10">
+    <div class="grid grid-cols-1 p-10">
+      <div class="flex flex-wrap content-start px-12">
+        <h3 class="p-4">My Review</h3>
+      </div>
       <div class="flex flex-wrap content-start px-4">
         <review></review>
       </div>
