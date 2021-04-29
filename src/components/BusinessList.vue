0e4c6eee29d0a444584a56afeb95d67929bc3a51
daerxi <bli3@unb.ca>
2021-04-29 09:51:56 -0300

    fix error message

--- src/components/BusinessList.vue
+++ src/components/BusinessList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="business in businesses" :key="business" class="flex flex-wrap p-4">
+    <div v-for="business in businesses" :key="i" class="flex flex-wrap p-4">
       <business v-bind:company-name="business.name" v-bind:photo="photo" v-bind:redirect-link="redirectLink"
                 v-bind:rateValue="rateValue"></business>
     </div>
