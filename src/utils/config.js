490578d6726eb386eae89ff8b594956f0d242156
daerxi <bli3@unb.ca>
2021-04-30 18:14:45 -0300

    update base_url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://localhost:4040"
+export const BASE_URL = "http://159.65.243.244:4040"
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    publicPath: '/'
+    devServer: {
+        allowedHosts: [
+            'ondigitalocean.app'
+        ]
+    }
 }
