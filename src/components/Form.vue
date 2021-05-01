6b2644756bad9b743041439abae8942de7e9337c
daerxi <bli3@unb.ca>
2021-05-01 14:51:36 -0300

    styling for headers

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-grey-lighter p-5 py-24 flex flex-col">
     <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-      <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-        <h1 class="mb-5 text-3xl text-center">{{ title }}</h1>
+      <div class="bg-white px-6 py-8 rounded shadow-md w-full">
+        <h2 class="mb-5 text-3xl text-center">{{ title }}</h2>
         <alert-component v-if="alertOpen" :type="type" :message="message"></alert-component>
         <slot></slot>
       </div>
