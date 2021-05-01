adc82b12d7d91b918c178b23aa6d0e8df065a012
daerxi <bli3@unb.ca>
2021-05-01 14:42:37 -0300

    hr styling

--- src/components/TCP.vue
+++ src/components/TCP.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="text-center text-sm text-gray-700 mt-6">
-    <div class="py-4"></div>
+    <hr>
+    <div class="py-3"></div>
     By signing up, you agree to the
     <a class="no-underline border-b border-grey-dark text-gray-600" href="#">
       Terms of Service
@@ -21,6 +22,7 @@ export default {
   hr{
     height: 1px;
     background-color: #718096;
+    opacity: .3;
     border: none;
   }
 </style>
