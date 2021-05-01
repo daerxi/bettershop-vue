577c2e7c988a2c94b0975fe09b4454b82c7e6771
daerxi <bli3@unb.ca>
2021-05-01 14:56:10 -0300

    styling

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-grey-lighter py-16 flex flex-col">
-    <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+    <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center">
       <div class="bg-white px-6 py-8 rounded shadow-md w-full">
         <h2 class="mb-5 text-3xl text-center">{{ title }}</h2>
         <alert-component v-if="alertOpen" :type="type" :message="message"></alert-component>
