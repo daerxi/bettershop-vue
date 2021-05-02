95d5c324abd27f21716f6243453e0995ff6b9e28
daerxi <bli3@unb.ca>
2021-05-02 15:37:15 -0300

    footer integration

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,7 @@
       <div class="flex flex-col lg:flex-row items-center w-full lg:w-auto">
         <div class="mx-auto lg:mx-0 lg:ml-auto">
           <a class="inline-block mt-0 ml-8 text-blue-900 hover:text-indigo-600" href="/donate">Donate</a>
-          <a class="inline-block mt-0 ml-8 text-blue-900 hover:text-indigo-600" href="#">Github</a>
+          <a class="inline-block mt-0 ml-8 text-blue-900 hover:text-indigo-600" href="https://github.com/daerxi/bettershop-vue">Github</a>
         </div>
         <div class="flex justify-center space-x-6 mt-4 lg:mt-0 lg:ml-8">
           <a href="#">
