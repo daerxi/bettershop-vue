aaf623e925d211d1ca1673deabca261bf8e201fa
daerxi <bli3@unb.ca>
2021-05-02 14:53:16 -0300

    fix business.vue styling

--- src/components/Business.vue
+++ src/components/Business.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="p-2">
-    <div class="grid grid-cols-10 p-2">
+  <section class="p-3">
+    <div class="grid grid-cols-11 p-2">
       <div class="col-span-2">
         <round-image class="w-48" :redirect="redirectLink" :user="user"/>
         <rate :editable="editable" v-bind:rateValue="rateValue"/>
       </div>
       <div class="col-span-2"/>
-      <div class="text-left w-full py-1 w-72 col-span-6">
+      <div class="text-left w-full py-1 w-72 col-span-7">
         <h3 class="text-xl font-semibold">{{ business.name }}</h3>
         <p class="text-sm italic">{{ business.category }}</p>
         <p class="text-gray-600">{{ business.description }}</p>
--- src/components/Review.vue
+++ src/components/Review.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="p-8">
-    <rate :editable="editable" v-bind:rateValue="review.rate"/>
-    <round-image :user="user" :redirect="redirectLink"/>
-    <p class="inline">{{ review.content }}</p>
+  <div class="p-4">
+    <section class="p-2">
+      <div class="grid grid-cols-11 p-2">
+        <div class="col-span-2">
+          <round-image class="w-48" :redirect="redirectLink" :user="user"/>
+          <rate :editable="editable" v-bind:rateValue="review.rate"/>
+        </div>
+        <div class="col-span-2"/>
+        <div class="text-left w-full py-1 w-72 col-span-7">
+<!--          <h3 class="text-xl font-semibold">{{ review }}</h3>-->
+          <p class="text-gray-600">{{ review.content }}</p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
