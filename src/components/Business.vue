c46fa1ee575a2426f5fe5c1b7eb8037fc0824c04
daerxi <bli3@unb.ca>
2021-05-02 14:48:27 -0300

    fix business.vue styling

--- src/components/Business.vue
+++ src/components/Business.vue
@@ -1,17 +1,15 @@
 <template>
-  <section class="p-4">
-    <div class="flex flex-wrap -mx-10">
-      <div class="grid grid-cols-3 p-2">
-        <div class="w-full py-1">
-          <round-image :redirect="redirectLink" :user="user"/>
-          <rate :editable="editable" v-bind:rateValue="rateValue"/>
-        </div>
-        <div class="w-full p-4"/>
-        <div class="w-full py-1">
-          <h3 class="text-left text-xl font-semibold">{{ business.name }}</h3>
-          <p class="text-left text-sm italic">{{ business.category }}</p>
-          <p class="text-left text-gray-600">{{ business.description }}</p>
-        </div>
+  <section class="p-2">
+    <div class="grid grid-cols-10 p-2">
+      <div class="col-span-2">
+        <round-image class="w-48" :redirect="redirectLink" :user="user"/>
+        <rate :editable="editable" v-bind:rateValue="rateValue"/>
+      </div>
+      <div class="col-span-2"/>
+      <div class="text-left w-full py-1 w-72 col-span-6">
+        <h3 class="text-xl font-semibold">{{ business.name }}</h3>
+        <p class="text-sm italic">{{ business.category }}</p>
+        <p class="text-gray-600">{{ business.description }}</p>
       </div>
     </div>
   </section>
