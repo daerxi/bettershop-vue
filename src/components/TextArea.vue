26b7aee63c3785f26975cb99bc81c37d16a37eb8
daerxi <bli3@unb.ca>
2021-05-03 13:20:38 -0300

    ui

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="py-2">
-    <textarea @keyup="countCharacters" class="block w-full text-black placeholder-gray-400
+    <textarea @keyup="countCharacters" class="block w-full placeholder-gray-400
                      transition duration-100 ease-in-out bg-white border
                      border-gray-300 rounded shadow-sm focus:border-blue-500
                      focus:ring-2 focus:ring-blue-500 focus:outline-none
                      focus:ring-opacity-50  disabled:opacity-50
-                     disabled:cursor-not-allowed h-48"
-              placeholder=" Enter your thoughts here..."
+                     disabled:cursor-not-allowed h-48 text-lg"
+              placeholder="Enter your thoughts here..."
               :value="value" @input="$emit('input', $event.target.value)">
     </textarea>
     <p class="text-right" v-bind:class="{'text-red-800':overLimit}">{{ characters }} characters remaining...</p>
