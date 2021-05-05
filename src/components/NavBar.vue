c5152bd6030d9a1d385f73204fb852545f854f23
daerxi <bli3@unb.ca>
2021-05-04 22:46:08 -0300

    add key for nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <nav class="relative select-none bg-bs-blue p-3 lg:flex lg:items-stretch w-full">
+    <nav :key="authenticated" class="relative select-none bg-bs-blue p-3 lg:flex lg:items-stretch w-full">
       <div class="flex flex-no-shrink items-stretch h-12">
         <a href="/"
            class="font-semibold text-xl flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center">BetterShop</a>
