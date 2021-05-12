6ef65975404ed8238f33ff8bbe8603dd22583d8f
daerxi <bli3@unb.ca>
2021-05-11 21:53:00 -0300

    FORM UI

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,7 +2,7 @@
   <div class="bg-grey-lighter px-2 py-16 flex flex-col">
     <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center">
       <div class="bg-white px-6 py-8 rounded shadow-md w-full">
-        <h2 class="mb-5 text-3xl font-semibold text-center">{{ title }}</h2>
+        <h2 class="mb-10 text-3xl font-semibold text-center">{{ title }}</h2>
         <form @submit.prevent="scroll">
           <alert-component v-if="alertOpen" id="alert" :type="type" :message="message"></alert-component>
           <slot></slot>
