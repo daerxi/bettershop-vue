e9ea9e69f0371952ddb9df1f0f6e6ccc069b9aed
daerxi <bli3@unb.ca>
2021-05-19 13:47:59 -0300

    fix empty token issue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -20,13 +20,18 @@ export default {
     }
   },
   async created() {
-    await UsersService.getMe().then(async r => {
-      this.user = r.data
-      if (!this.user.isBusiness)
-        this.redirectLink = "/profile/" + this.user.id
-      else
-        this.redirectLink = "/businesses/" + this.user.business.id
-    }).catch(() => this.redirectLink = "/login")
+    if (this.$cookies.get('authenticated') === "true") {
+      await UsersService.getMe().then(async r => {
+        this.user = r.data
+        if (!this.user.isBusiness)
+          this.redirectLink = "/profile/" + this.user.id
+        else
+          this.redirectLink = "/businesses/" + this.user.business.id
+      }).catch(() => this.redirectLink = "/login")
+    } else {
+      this.user.id = 0
+      this.user.avatar = null
+    }
   }
 }
 </script>
--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 // export const BASE_URL = "http://159.65.243.244:4040"
 
 /** local direct */
-export const BASE_URL = "http://192.168.2.58:4040"
+// export const BASE_URL = "http://192.168.2.58:4040"
 
 /** proxy */
-// export const BASE_URL = "/api"
+export const BASE_URL = "/api"
