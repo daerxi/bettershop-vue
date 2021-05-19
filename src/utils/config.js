e78b40da64568cb1ac9732c56802c29eaaa5feda
daerxi <bli3@unb.ca>
2021-05-18 23:50:29 -0300

    add proxy url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,8 @@
-
+/** prod direct */
 // export const BASE_URL = "http://159.65.243.244:4040"
-// export const BASE_URL = "http://159.65.243.244:80/api"
 
-// local proxy
-// export const BASE_URL = "http://192.168.2.58:8081/api"
-export const BASE_URL = "http://localhost:8081/api"
-
-// local direct
+/** local direct */
 // export const BASE_URL = "http://192.168.2.58:4040"
+
+/** proxy */
+export const BASE_URL = "/api"
