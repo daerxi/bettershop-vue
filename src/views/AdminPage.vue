22b25d32a40af3c6dc0db7a00ca9bfd03f165095
daerxi <bli3@unb.ca>
2021-05-19 20:54:56 -0300

    add html2pdf

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
+    <h1 class="py-6 font-semibold text-2xl" id="title">Admin Statistical Analysis System</h1>
+    <div class="px-24">
+    </div>
     <div class="px-24">
       <dropdown-component :fn="onDataChange" v-model="selectedData" :options="dataOption"
                           placeholder="Select type of data"/>
+      <div class="py-4"/>
+      <dropdown-component v-model="selectedChart" :options="chartOption" placeholder="Select type of chart"/>
+      <div class="py-4"/>
+      <button class="bg-gray-200 p-3 w-full rounded text-center hover:bg-gray-300 focus:outline-none"
+              @click="exportToPDF">Export to PDF
+      </button>
     </div>
     <div class="py-4"/>
-    <div class="px-24">
-      <dropdown-component v-model="selectedChart" :options="chartOption" placeholder="Select type of chart"/>
-    </div>
-    <div class="p-10"></div>
-    <div v-if="chartData.labels[0]">
+    <div v-if="chartData.labels[0]" ref="chart">
       <bar-chart v-if="selectedChart === 'Bar'" :chart-data="chartData" :fn="barStyle"></bar-chart>
       <pie-chart v-if="selectedChart === 'Pie'" :chart-data="chartData" :fn="barStyle"></pie-chart>
       <doughnut-chart v-if="selectedChart === 'Doughnut'" :chart-data="chartData" :fn="barStyle"></doughnut-chart>
       <polar-area-chart v-if="selectedChart === 'Polar Area'" :chart-data="chartData" :fn="barStyle"></polar-area-chart>
       <line-chart v-if="selectedChart === 'Line'" :chart-data="chartData" :fn="lineStyle"></line-chart>
+      <div class="p-12"></div>
     </div>
   </div>
 </template>
@@ -28,6 +34,7 @@ import PieChart from "@/components/PieChart";
 import DoughnutChart from "@/components/DoughnutChart";
 import PolarAreaChart from "@/components/PolarAreaChart";
 import DropdownComponent from "@/components/Dropdown";
+import html2pdf from 'html2pdf.js'
 
 export default {
   name: "AdminPage",
@@ -72,6 +79,15 @@ export default {
           this.chartData.datasets[0].data.push(this.businessCount[category.type])
         }
       }
+    },
+    exportToPDF() {
+      window.scrollTo(0,0)
+      html2pdf(this.$refs.chart, {
+        margin: 1,
+        filename: `${this.selectedData}_${this.selectedChart}_chart.pdf`,
+        image: {type: 'jpeg', quality: 1},
+        jsPDF: {unit: 'cm', format: 'letter', orientation: 'landscape'}
+      })
     }
   }
 }
